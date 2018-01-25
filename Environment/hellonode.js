3b4de732adf63ae38653b7d1ba93dd3dec10ca12
Stefan Christiansson <stefan.christiansso@learnet.se>
2018-01-25 12:47:01 +0100

    installed express

--- Environment/hellonode.js
+++ Environment/hellonode.js
@@ -6,4 +6,4 @@ http.createServer(function (request, response) {
     response.end('Hello World!\n');
 }).listen(8080);
 
-console.log('Srever running at http://127.0.0.1:8080');
+console.log('Server running at http://127.0.0.1:8080');
